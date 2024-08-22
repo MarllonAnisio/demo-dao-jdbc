79888566643f7cd771b3d8318b08c2331ca114cd
M. ANISIO <marllon.anisio@hotmail.com>
2024-08-22 14:30:42 -0300

    DepartmentDao, SellerDao interfaces and models

---
+++ src/application/main.java
@@ -0,0 +1,27 @@
+package application;
+
+
+
+import java.util.Date;
+
+import model.Department;
+import model.Seller;
+
+public class main {
+
+	public static void main(String[] a) {
+		
+		
+		
+		Department obj = new Department(1, "Books");
+		
+		Seller seller = new Seller( 21, "Bob", "Bob@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller.toString());
+		
+		
+		
+		
+		
+	}
+}
