1923abfb02489d35712cdfc8bcdbc72cd6cf5d99
Marllon Anisio Martins Camelo <135665212+MarllonAnisio@users.noreply.github.com>
2024-08-25 21:41:01 -0300

    Add files via upload

---
+++ DemoDaoJdbc/src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,33 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.Seller;
+import model.dao.SellerDao;
+
+public class SellerDaoJDBC implements SellerDao{
+
+
+	public void insert(Seller obj) {
+			
+	}
+	public void update(Seller obj) {
+		
+		
+	}
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Seller findById(Integer id) {
+		
+		return null;
+	}
+
+	public List<Seller> findAll() {
+	
+		return null;
+	}
+
+}
