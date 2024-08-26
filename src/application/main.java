6b85aea369ea1015296172347f2a70a7e5253181
Marllon Anisio <marllon.anisio@hotmail.com>
2024-08-26 16:34:16 -0300

    Reusing instantiation

--- src/application/main.java
+++ src/application/main.java
@@ -14,10 +14,10 @@ public class main {
 		Department obj = new Department(1, "Books");
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+		System.out.println("==== TEST 1: Seller findById  ==== ");
 		Seller seller = sellerDao.findById(3);
 	
 		System.out.println(seller);
-		
+		System.out.println("=============  END  ============== ");
 	}
 }
